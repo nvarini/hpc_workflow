fd0961c785555049c84bf68366ac021f080240ab
nvarini <nicola.varini@gmail.com>
2017-11-08 15:16:16 +0100

    Fixed issue in submit_benchmarks.py

--- submit/submit_benchmarks.py
+++ submit/submit_benchmarks.py
@@ -20,15 +20,15 @@ UpfData = DataFactory('upf')
 ParameterData = DataFactory('parameter')
 KpointsData = DataFactory('array.kpoints')
 
-
+with open(args.json_hpc) as data_file:    
+   json_hpc = json.load(data_file)
+ 
 qb = QueryBuilder()
 qb.append(JobCalculation,tag="mycalculation",project=["*"])
-qb.append(Group,filters={"name":"pw_scf_final_test_bench_small_v2"},group_of="mycalculation")
+qb.append(Group,filters={"name":json_hpc["query_group"]},group_of="mycalculation")
 calcs_list=qb.all()
 
-
-
-pseudo_family = 'sssp_eff_pbe_2'
+pseudo_family = json_hpc['pseudo']
 structures_wf = []
 kpoints_wf = []
 pw_parameters_wf = []
@@ -36,40 +36,38 @@ hpc_workflow_params = {}
 keys = []
 count=0
 
-with open(args.json_hpc) as data_file:    
-   data_benchmark = json.load(data_file)
-   for i in calcs_list[0:1]:
-   #for i in calcs_list:
-        calc = i[0]
-        struct = calc.get_inputs_dict()['structure']
-        kpoints = KpointsData()
-        kmesh =  calc.get_inputs_dict()['kpoints'].get_kpoints_mesh()
-        kpoints_wf.append(kmesh)
-        kpoints.store()
-        structures_wf.append(struct)
-        scaled_struct, new_k = scale_structure(kmesh,struct,data_benchmark['scale'])
-        scaled_struct.store()
-	param =  calc.get_inputs_dict()['parameters'].get_attrs()
-        
-        parameters = set_dict(param,args.json_pw,1,1)
-        pw_parameters = ParameterData(dict=parameters)
-        pw_parameters.store()
-        pw_parameters_wf.append(pw_parameters)
-        hpc_parameters =  ParameterData(dict=data_benchmark)
-        hpc_parameters.store()
+for i in calcs_list[0:1]:
+#for i in calcs_list:
+     calc = i[0]
+     struct = calc.get_inputs_dict()['structure']
+     kpoints = KpointsData()
+     kmesh =  calc.get_inputs_dict()['kpoints'].get_kpoints_mesh()
+     kpoints_wf.append(kmesh)
+     kpoints.store()
+     structures_wf.append(struct)
+     scaled_struct, new_k = scale_structure(kmesh,struct,json_hpc['scale'])
+     scaled_struct.store()
+     param =  calc.get_inputs_dict()['parameters'].get_attrs()
+     
+     parameters = set_dict(param,args.json_pw,1,1)
+     pw_parameters = ParameterData(dict=parameters)
+     pw_parameters.store()
+     pw_parameters_wf.append(pw_parameters)
+     hpc_parameters =  ParameterData(dict=json_hpc)
+     hpc_parameters.store()
 
-        code = test_and_get_code(args.code, expected_code_type='quantumespresso.pw')
-        UpfData.get_upf_group(pseudo_family)
-        hpc_workflow_params.update({'pw_codename_'+str(count): args.code,
-                   'structure_'+str(count): struct,
-                   'hpc_params_'+str(count): hpc_parameters,
-                   'pseudo_family_'+str(count): pseudo_family,
-                   'kpoints_'+str(count):kmesh,
-                   'pw_parameters_'+str(count): pw_parameters,
-                   })
-        keys.append(count)
-        count+=1
- 
+     code = test_and_get_code(args.code, expected_code_type='quantumespresso.pw')
+     UpfData.get_upf_group(pseudo_family)
+     hpc_workflow_params.update({'pw_codename_'+str(count): args.code,
+                'structure_'+str(count): struct,
+                'hpc_params_'+str(count): hpc_parameters,
+                'pseudo_family_'+str(count): pseudo_family,
+                'kpoints_'+str(count):kmesh,
+                'pw_parameters_'+str(count): pw_parameters,
+                })
+     keys.append(count)
+     count+=1
+
 wf_params = {}
 bench = BenchWorkflow()
 bench.set_params(hpc_workflow_params)
